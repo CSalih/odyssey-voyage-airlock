ac67975f1247afdfac80ba9c42761b43d9f8b1e6
Salih Candir <salih@live.at>
2022-09-29 22:26:31 +0200

    refactor: move index.js to monolith/datasources

---
+++ monolith/datasources/index.js
@@ -0,0 +1,11 @@
+const BookingsDataSource = require('./bookings');
+const ReviewsDataSource = require('./reviews');
+const ListingsAPI = require('./listings');
+const PaymentsAPI = require('./payments');
+
+module.exports = {
+    BookingsDataSource,
+    ReviewsDataSource,
+    ListingsAPI,
+    PaymentsAPI,
+};
