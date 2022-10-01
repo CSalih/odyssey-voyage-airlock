70c79c2176acb9026bafa147908adadc990442a0
Salih Candir <salih@live.at>
2022-10-01 22:29:36 +0200

    feat: add payments subgraph

---
+++ packages/airlock-subgraph-payments/index.js
@@ -0,0 +1,42 @@
+const { ApolloServer, gql, AuthenticationError} = require('apollo-server');
+const { buildSubgraphSchema } = require('@apollo/subgraph');
+const { readFileSync } = require('fs');
+const axios = require('axios');
+
+const typeDefs = gql(readFileSync('./schema.graphql', { encoding: 'utf-8' }));
+const resolvers = require('./resolvers');
+const PaymentsAPI = require("./datasources/payments");
+
+const server = new ApolloServer({
+  schema: buildSubgraphSchema({ typeDefs, resolvers }),
+  dataSources: () => {
+    return {
+      paymentsAPI: new PaymentsAPI(),
+    };
+  },
+  context: async ({req}) => {
+    const token = req.headers.authorization || '';
+    const userId = token.split(' ')[1]; // get the user name after 'Bearer '
+    if (userId) {
+      const { data } = await axios
+          .get(`http://localhost:4011/login/${userId}`)
+          .catch((error) => {
+            throw new AuthenticationError(error.message);
+          });
+
+      return { userId: data.id, userRole: data.role };
+    }
+  },
+});
+
+const port = 4006;
+const subgraphName = 'payments';
+
+server
+  .listen({ port })
+  .then(({ url }) => {
+    console.log(`🚀 Subgraph ${subgraphName} running at ${url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ packages/airlock-subgraph-payments/resolvers.js
@@ -0,0 +1,33 @@
+const { AuthenticationError } = require('apollo-server');
+const authErrMessage = '*** you must be logged in ***';
+
+const resolvers = {
+  Mutation: {
+    addFundsToWallet: async (_, {amount}, {dataSources, userId}) => {
+      if (!userId) throw new AuthenticationError(authErrMessage);
+      try {
+        const updatedWallet = await dataSources.paymentsAPI.addFunds({userId, amount});
+        return {
+          code: 200,
+          success: true,
+          message: 'Successfully added funds to wallet',
+          amount: updatedWallet.amount,
+        };
+      } catch (err) {
+        return {
+          code: 400,
+          success: false,
+          message: err.message,
+        };
+      }
+    },
+  },
+  Guest: {
+    funds: async (_, __, {dataSources, userId}) => {
+      const {amount} = await dataSources.paymentsAPI.getUserWalletAmount(userId);
+      return amount;
+    },
+  }
+};
+
+module.exports = resolvers;
